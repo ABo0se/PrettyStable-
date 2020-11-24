d499712aac7658f006869c1fb10049d18b01e57b
ABoOse <thanawat.65938@mail.kmutt.ac.th>
2020-11-24 19:35:34 +0700

    Stable

--- Page1.xaml.cs
+++ Page1.xaml.cs
@@ -36,12 +36,6 @@ namespace Primecanva
 
         }
 
-        private void GoPage2(object sender, RoutedEventArgs e)
-        {
-            Page2 p2 = new Page2();
-            this.NavigationService.Navigate(p2);
-        }
-
         private void primefinder(object sender, RoutedEventArgs e)
         {
             FindPrimeResult();
